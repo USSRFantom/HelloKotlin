881b571f4925edbbf5d5610568d1de0167ab8acd
Ussrfantom <dimkakrohin@mail.ru>
2020-08-16 13:45:50 +0300

    Nullable types

--- src/kot/Main.kt
+++ src/kot/Main.kt
@@ -1,8 +1,13 @@
 package kot
 
 
+private var name1: String? = null
+private var name2: String? = "12312312"
+private var name3: String? = "2323232"
+
 fun main() {
-        val name = "Ivan"
-        var age = 1
-        println()
+var length = (name1?.length?:0) + (name2?.length?:0) + (name3?.length ?:0)
+        println(length)
 }
+
+
